22419630052fc6ac3491a17cc322b4d73ddfc5a2
hosseincollege <hosseincollege@gmail.com>
2025-08-15 14:18:32 +0330

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
-      <h1>پلتفرم کارگاه‌ها - نسخه MVP</h1>
-      <p>این نسخه آزمایشی است و فقط برای نمایش آنلاین ساخته شده.</p>
-    </div>
-  );
-}
